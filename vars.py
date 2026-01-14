4a77e199b99beaf503d8381940a5802c0cdd426b
decorb <decorb18@gmail.com>
2026-01-14 16:05:10 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,7 +7,7 @@ API_ID = int(environ.get("API_ID", "21437108"))
 API_HASH = environ.get("API_HASH", "53b13c686a0cce30a8aa35710ac11a62")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "7714608838"))
+OWNER = int(environ.get("OWNER", "8447066074"))
 CREDIT = environ.get("CREDIT", "♡⎯꯭̽𝅃꯭᳚🦋 ∂я-αѕρ 𝅥ͦ𝆬⋆𝆹𝅥𝅮᪳🪽")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765,8447066074').split(',')
@@ -20,4 +20,5 @@ if int(OWNER) not in AUTH_USERS:
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
